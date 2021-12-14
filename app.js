6d80747391e63764b4da6493a5a7eb0c7f234a68
Faisal Iqbal <faisaliqbal012@gmail.com>
2021-12-13 23:31:32 -0800

    Third Commit

--- app.js
+++ app.js
@@ -1,10 +1,46 @@
 const bar = document.querySelector("#bar");
 const menu = document.querySelector(".menu");
 
-bar.addEventListener("click", function(){
-    // console.log("You Clicked me");
-    menu.classList.toggle("menuHide");
-    bar.classList.toggle("barRotate");
-})
+bar.addEventListener("click", function () {
+  // console.log("You Clicked me");
+  menu.classList.toggle("menuHide");
+  bar.classList.toggle("barRotate");
+});
 
+const gridbtn1 = document.getElementById("gridbtn1");
+const gridbtn2 = document.getElementById("gridbtn2");
+const gridImage = document.getElementById("gridImage");
 
+let imageCount = 1;
+let imagePath;
+
+gridbtn2.addEventListener("click", () => {
+  imageCount++;
+  console.log("hello");
+  if (imageCount <= 5) {
+    imagePath = "./pictures/transition" + imageCount + ".jpg";
+    console.log(imagePath);
+    gridImage.src = imagePath;
+  }
+  if (imageCount > 5) {
+    imageCount = 1;
+    imagePath = "./pictures/transition" + imageCount + ".jpg";
+    console.log(imagePath);
+    gridImage.src = imagePath;
+  }
+});
+
+gridbtn1.addEventListener("click", () => {
+  imageCount--;
+  if (imageCount >= 1) {
+    imagePath = "./pictures/transition" + imageCount + ".jpg";
+    console.log(imagePath);
+    gridImage.src = imagePath;
+  }
+  if (imageCount < 1) {
+    imageCount = 5;
+    imagePath = "./pictures/transition" + imageCount + ".jpg";
+    console.log(imagePath);
+    gridImage.src = imagePath;
+  }
+});
